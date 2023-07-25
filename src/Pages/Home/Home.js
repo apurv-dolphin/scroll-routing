b67806366f2c335c8f4b36c5530ed38ae4f9a62a
apurv-dolphin <apurv.dolphinwebsolution@gmail.com>
2023-07-25 14:22:43 +0530

    add custom input design.

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,6 +5,10 @@ export default function Home() {
     <div id="home">
       <h1>KHALAS APURV</h1>
       <div className="myname">𝒜𝓅𝓊𝓇𝓋</div>
+      <div class="input-box">
+        <input type="text" autofocus="autofocus" />
+        <span class="unit">£</span>
+      </div>
     </div>
   );
 }
